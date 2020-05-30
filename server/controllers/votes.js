6bef336c3878aae659198516d9de7b20740eb831
salih <salih.ozdemir@metropolsoftware.com>
2020-05-30 16:52:40 +0300

    create votes controller

---
+++ server/controllers/votes.js
@@ -0,0 +1,14 @@
+exports.upvote = async (req, res) => {
+  const post = await req.post.vote(req.user.id, 1);
+  res.json(post);
+};
+
+exports.downvote = async (req, res) => {
+  const post = await req.post.vote(req.user.id, -1);
+  res.json(post);
+};
+
+exports.unvote = async (req, res) => {
+  const post = await req.post.vote(req.user.id, 0);
+  res.json(post);
+};
