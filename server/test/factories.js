5131f8d44b0d8e1a84bc73d45f941a2a7cb929c1
salih <salih.ozdemir@metropolsoftware.com>
2020-06-05 12:54:53 +0300

    add test scenarios for post

--- server/test/factories.js
+++ server/test/factories.js
@@ -4,3 +4,11 @@ exports.validUser = () => ({
   username: faker.name.firstName().toLowerCase(),
   password: 'password'
 });
+
+exports.validPost = (author, category) => ({
+  title: faker.lorem.sentence(),
+  url: faker.internet.url(),
+  category,
+  author,
+  type: 'link'
+});
